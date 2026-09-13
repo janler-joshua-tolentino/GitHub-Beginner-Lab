cfda6a74a6637cf7f508cada16e7caadbf6c9a12
Tolentino, Janler Joshua (Student) <tolentino.395606@balayan.sti.edu.ph>
2026-09-14 00:03:52 +0800

    Resolve merge conflict and keep Year Level 4

--- StudentProfile/StudentProfile/Form1.Designer.cs
+++ StudentProfile/StudentProfile/Form1.Designer.cs
@@ -68,19 +68,15 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< HEAD
+
             this.label4.Location = new System.Drawing.Point(285, 359);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(140, 25);
             this.label4.TabIndex = 3;
             this.label4.Text = "Year Level: 4";
-=======
-            this.label4.Location = new System.Drawing.Point(293, 353);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(140, 25);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Year Level: 2";
->>>>>>> origin/main
+
+            
+
             // 
             // Form1
             // 
